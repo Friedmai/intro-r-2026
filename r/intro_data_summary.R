6b8aa1985f9364c8b17a06713adb99c59a4bef00
FRIEDMAI <FriedmaI@trimet.org>
2026-03-19 11:15:52 -0700

    Day 2 updated r file with filter practice

--- r/intro_data_summary.R
+++ r/intro_data_summary.R
@@ -23,3 +23,21 @@ tail(raw_15min) #gives final 6 rows
 summary(raw_15min) #provides aggregate data about each column
 glimpse(raw_15min) #nicer version of str()
 dfSummary(raw_15min) #another nice summary format
+
+raw_15min[3,2] # in brackets, first variable is row number, second is column. Better to use named columns
+mean(raw_15min$speed) #won't work because there are NA values in column
+mean(raw_15min$volume) #this works because column is all integer values
+hist(raw_15min$volume) #shows basic distribution of values in bar graph (see Plots)
+
+# integrating filters
+
+occ_20plus <- raw_15min |> 
+  filter(occupancy > 20)
+  
+  # the end of line 34 is the "pipe". can also do %>%, also with ctrl+shift+m
+
+#multiple filters
+
+occ10_spd80 <- raw_15min |> 
+  filter(occupancy < 10 & speed > 80)
+table(occ10_spd80$detector_id) # this table function shows how many times each value appears in stated column
